327d1d3c1a20013a506ca1e1916aa1fb897a5e75
Daniele Pintore <daniele.pintore1@gmail.com>
2024-07-15 00:35:58 +0200

    Remove packer references

--- .config/nvim/lua/configs/init.lua
+++ .config/nvim/lua/configs/init.lua
@@ -1,5 +1,4 @@
 require("configs.remap") -- set remps like leader and other useful ones
 require("configs.set") -- set vim preferences
--- require("configs.packer")
 require("configs.lazy") -- configure lazy plugin manager
 require("configs.templates") -- configure templates for various files
