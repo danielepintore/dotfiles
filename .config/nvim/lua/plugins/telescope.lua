283e56e90624adbaecd8b54d5e0adb600d6a7a78
Daniele Pintore <daniele.pintore1@gmail.com>
2024-07-15 00:22:06 +0200

    Now jump to references is displayed with telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
-	"nvim-telescope/telescope.nvim", tag = "0.1.5",
-	config = function ()
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	config = function()
 		require('telescope').setup({
 			defaults = {
 				mappings = {
@@ -14,10 +15,11 @@ return {
 		})
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>ps', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") });
 		end)
+		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+		vim.keymap.set('n', 'gr', builtin.lsp_references)
 	end,
 	dependencies = { "nvim-lua/plenary.nvim" }
 }
