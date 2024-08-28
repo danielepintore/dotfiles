18e64b4b9d545be726b684ab5b9699dbd53e450b
Daniele Pintore <daniele.pintore1@gmail.com>
2024-08-28 11:03:50 +0200

    Now hyprlock uses current wallpaper as background

--- .config/hypr/change_wallpaper.sh
+++ .config/hypr/change_wallpaper.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-swaybg -i $(find ~/.config/hypr/wallpapers/ -type f | shuf -n1) &
+mkdir -p ~/.config/hypr/hyprlock/
+wallpaper=$(find ~/.config/hypr/wallpapers/ -type f | shuf -n1)
+swaybg -i $wallpaper &
+# Update hyprlock background
+cp $wallpaper ~/.config/hypr/hyprlock/background.jpg
 OLD_PID=$!
 while true; do
+		wallpaper=$(find ~/.config/hypr/wallpapers/ -type f | shuf -n1)
     sleep 600
-	swaybg -i $(find ~/.config/hypr/wallpapers/ -type f | shuf -n1) &
+		swaybg -i $wallpaper &
+		# Update hyprlock background
+		cp $wallpaper ~/.config/hypr/hyprlock/background.jpg
     NEXT_PID=$!
     sleep 5
     kill $OLD_PID
