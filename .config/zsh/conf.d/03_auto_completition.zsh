5dafef074f6467e746559aed6e160130b157804c
Daniele Pintore <daniele.pintore1@gmail.com>
2025-07-12 01:43:32 +0200

    now config alias is a function, autocomplete for paths is now working

--- .config/zsh/conf.d/03_auto_completition.zsh
+++ .config/zsh/conf.d/03_auto_completition.zsh
@@ -7,8 +7,13 @@ if [[ -o interactive ]]; then
     zstyle ':completion:*' menu select
     zmodload zsh/complist
     compinit
-    _comp_options+=(globdots)		# Include hidden files.
-    unset -f setup_completion  # cleanup the function
+    _comp_options+=(globdots) # Include hidden files.
+    unset -f setup_completion # cleanup the function
+
+    # dotfiles manager autocompletition trick for add and rm command
+    # definition on the config functions file (functions/config.zsh)
+    compdef _config config
+
     # Use vim keys in tab complete menu:
     bindkey -M menuselect 'h' vi-backward-char
     bindkey -M menuselect 'k' vi-up-line-or-history
