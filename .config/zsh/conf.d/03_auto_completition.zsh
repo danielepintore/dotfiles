57929e73828d1bd0258191dcf31c43ed7f55d6f1
Daniele Pintore <daniele.pintore1@gmail.com>
2025-07-12 00:41:21 +0200

    lint zsh config

--- .config/zsh/conf.d/03_auto_completition.zsh
+++ .config/zsh/conf.d/03_auto_completition.zsh
@@ -7,14 +7,14 @@ if [[ -o interactive ]]; then
     zstyle ':completion:*' menu select
     zmodload zsh/complist
     compinit
-		_comp_options+=(globdots)		# Include hidden files.
+    _comp_options+=(globdots)		# Include hidden files.
     unset -f setup_completion  # cleanup the function
-		# Use vim keys in tab complete menu:
-		bindkey -M menuselect 'h' vi-backward-char
-		bindkey -M menuselect 'k' vi-up-line-or-history
-		bindkey -M menuselect 'l' vi-forward-char
-		bindkey -M menuselect 'j' vi-down-line-or-history
-  }
+    # Use vim keys in tab complete menu:
+    bindkey -M menuselect 'h' vi-backward-char
+    bindkey -M menuselect 'k' vi-up-line-or-history
+    bindkey -M menuselect 'l' vi-forward-char
+    bindkey -M menuselect 'j' vi-down-line-or-history
+}
   precmd_functions+=(setup_completion)
 fi
 
