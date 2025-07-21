ee468c25f62c88dfbea9f76c919c6b16791a2025
Daniele Pintore <daniele.pintore1@gmail.com>
2025-07-21 10:44:49 +0200

    vim: map leader<hjkl> to move between splits

--- .config/nvim/lua/configs/remap.lua
+++ .config/nvim/lua/configs/remap.lua
@@ -31,8 +31,10 @@ vim.keymap.set("n", "<leader>r", "e!")
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
