eb419091c75ad05a82a5578526d777fdb0874755
Daniele Pintore <daniele.pintore1@gmail.com>
2025-07-29 19:56:45 +0200

    add docker auto completions

--- .config/zsh/conf.d/03_auto_completition.zsh
+++ .config/zsh/conf.d/03_auto_completition.zsh
@@ -2,13 +2,11 @@
 
 # Auto completition
 if [[ -o interactive ]]; then
-  function setup_completion {
     autoload -U compinit
     zstyle ':completion:*' menu select
     zmodload zsh/complist
     compinit
     _comp_options+=(globdots) # Include hidden files.
-    unset -f setup_completion # cleanup the function
 
     # dotfiles manager autocompletition trick for add and rm command
     # definition on the config functions file (functions/config.zsh)
@@ -19,7 +17,4 @@ if [[ -o interactive ]]; then
     bindkey -M menuselect 'k' vi-up-line-or-history
     bindkey -M menuselect 'l' vi-forward-char
     bindkey -M menuselect 'j' vi-down-line-or-history
-}
-  precmd_functions+=(setup_completion)
 fi
-
--- .config/zsh/conf.d/_env.zsh
+++ .config/zsh/conf.d/_env.zsh
@@ -11,6 +11,8 @@ path+=$ANDROID_HOME/tools
 path+=$ANDROID_HOME/tools/bin
 path+=$ANDROID_HOME/platform-tools
 
+export FPATH="$ZDOTDIR/completions:$FPATH"
+
 export EDITOR=nvim
 
 export QT_QPA_PLATFORMTHEME=qt5ct
