c822c45ea064b705768c15569beadde0278d7ae7
Daniele Pintore <daniele.pintore1@gmail.com>
2026-04-14 09:51:12 +0200

    nvim: use rose-pine theme

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,36 +1,42 @@
+-- Custom colorscheme application function that disables background colors for
+-- Normal and NormalFloat highlights
 function ApplyColorscheme(color)
-	color = color or "default"
-	vim.cmd.colorscheme(color)
+    color = color or "default"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require('catppuccin').setup({
-				term_colors = false,
-				transparent_background = true,
-				styles = {
-					comments = {},
-					conditionals = {},
-					loops = {},
-					functions = {},
-					keywords = {},
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-				},
-			})
-
-			ApplyColorscheme("catppuccin")
-		end
-	},
+    {
+        "catppuccin/nvim",
+        enabled = false,
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            transparent_background = true,
+        },
+        config = function(_, opts)
+            require("catppuccin").setup(opts)
+            vim.cmd("colorscheme catppuccin")
+        end
+    },
+    {
+        "rose-pine/neovim",
+        enabled = true,
+        lazy = false,
+        name = "rose-pine",
+        priority = 1000,
+        opts = {
+            styles = {
+                transparency = true
+            },
+        },
+        config = function(_, opts)
+            require("rose-pine").setup(opts)
+            vim.cmd("colorscheme rose-pine")
+        end
+    },
 }
-
