a24ffeb5c89bd15592e00c43961f412ba7800caf
Daniele Pintore <daniele.pintore1@gmail.com>
2026-05-14 15:20:19 +0200

    update hyprland border color

--- .config/hypr/lua/theme.lua
+++ .config/hypr/lua/theme.lua
@@ -17,7 +17,7 @@ hl.config({
         border_size = 1,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+            active_border   = "rgba(ffffffff)",
             inactive_border = "rgba(595959aa)",
         },
 
