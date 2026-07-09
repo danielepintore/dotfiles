10b57ef30c6566d36aa1e19450ff8be5400a4fe8
Daniele Pintore <daniele.pintore1@gmail.com>
2026-07-09 09:59:25 +0200

    hyprland: add description for thinkpad monitor

--- .config/hypr/lua/monitors.lua
+++ .config/hypr/lua/monitors.lua
@@ -2,6 +2,7 @@
 ---- MONITORS ----
 ------------------
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+
 hl.monitor({
     output   = "desc:ASUSTek COMPUTER INC XG27AQWMG W2LMTF013105",
     mode     = "2560x1440@280",
@@ -18,8 +19,9 @@ hl.monitor({
     scale    = "auto",
 })
 
+-- Thinkpad x1 carbon monitor
 hl.monitor({
-    output   = "eDP-1",
+    output   = "desc:BOE 0x094C",
     mode     = "1920x1200@60",
     position = "auto",
     scale    = "1.00",
